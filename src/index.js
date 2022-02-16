fd24d57ab775c211376ecef46cd3a5d4c06b7ff4
karol <karol.rudzinski1990@gmail.com>
2022-02-16 16:00:24 +0100

    repo

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from 'views/Root';
+import 'assets/styles/fonts.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
